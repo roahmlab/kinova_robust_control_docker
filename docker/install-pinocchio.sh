c696d1e068cf004460ae99da6dce284222f52982
Cfather <jimzhang@umich.edu>
2024-12-11 03:08:50 +0000

    humble attempt

--- docker/install-pinocchio.sh
+++ docker/install-pinocchio.sh
@@ -8,5 +8,5 @@ apt install -qqy robotpkg-py3*-pinocchio
 export PATH=/opt/openrobots/bin:$PATH
 export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/opt/openrobots/lib/python3.8/site-packages:$PYTHONPATH
+export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH
 export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
